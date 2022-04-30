0655b6ca25b6a797ab167c885560bf844b0ea76d
Abu Kasozi <abukasozi@gmail.com>
2022-04-30 15:33:53 +0300

    Added implementatio of a TreeNode

---
+++ src/main/java/io/datastructures/trees/TreeNode.java
@@ -0,0 +1,26 @@
+package io.datastructures.trees;
+
+/**
+ * @author Abu Bizibu
+ * @created
+ * @project
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
