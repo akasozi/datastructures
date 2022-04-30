060a751d11a4c5be347d9836408f202fdfd2b333
Abu Kasozi <abukasozi@gmail.com>
2022-04-30 15:45:24 +0300

    Preorder traversal of a binary tree

---
+++ src/main/java/io/datastructures/trees/PreorderTraversalTest.java
@@ -0,0 +1,41 @@
+package io.datastructures.trees;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Abu Bizibu
+ * @created
+ * @project
+ */
+public class PreorderTraversalTest {
+
+
+    public static void main(String[] args) {
+           // input: [1, null, 2, 3]
+           // output: [1, 2, 3]
+           TreeNode root = new TreeNode();
+           root.val = 1;
+           root.right = new TreeNode(2, new TreeNode(3), null);
+           root.left = null;
+           List<Integer> result = preorderTraversal(root);
+           System.out.println(Arrays.toString((Object[])result.toArray()));
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+         List<Integer> list = new ArrayList<>();
+         preorderTraversal(root, list);
+         return list;
+    }
+
+    // recuirsive!
+    public static void preorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        // root -> left -> right
+        list.add(root.val);
+        preorderTraversal(root.left, list);
+        preorderTraversal(root.right, list);
+    }
+}
